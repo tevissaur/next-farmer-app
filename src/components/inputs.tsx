d61d202a049c4424cb9b050623684e47443d5503
Tevis Reilly <treilly@blainsupply.com>
2023-04-21 10:03:27 -0500

    adding filler to empty pages

--- src/components/inputs.tsx
+++ src/components/inputs.tsx
@@ -13,12 +13,12 @@ export const DefaultStyleInput = ({
   return (
     <input
       type={type}
-      className={`m-1 translate-x-reverse-0.5 translate-y-reverse-0.5 rounded-full border-1 border-black bg-black/25 px-4 py-2 font-bold text-black shadow-outline transition-all duration-200 ${className || ""}`}
+      className={`m-1 translate-x-reverse-0.5 translate-y-reverse-0.5 rounded-full border-1 border-black bg-black/25 px-4 py-2 text-black shadow-outline transition-all duration-200 ${className || ""}`}
       placeholder={placeholder}
     />
   );
 };
 
 export const SearchBar = () => {
-  return <DefaultStyleInput type="text" placeholder="Search for anything!" className="w-full focus-visible:outline-none" />;
+  return <DefaultStyleInput type="text" placeholder="Search for anything!" className="w-full focus-visible:outline-none font-bold" />;
 };
---
+++ src/pages/cart.tsx
@@ -0,0 +1,32 @@
+import { useUser } from "@clerk/nextjs";
+import { type NextPage } from "next";
+
+import { useEffect } from "react";
+import {
+  SignInButtonStyled,
+  SignOutButtonStyled,
+  SignUpButtonStyled,
+} from "~/components/buttons";
+import { Layout } from "~/components/layout";
+
+import { api } from "~/utils/api";
+
+const Cart: NextPage = () => {
+  const { data, isLoading, isSuccess, isError } = api.farms.getAll.useQuery();
+
+  const user = useUser();
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  return (
+    <>
+      {data &&
+        isSuccess &&
+        data.map((farm) => <div key={farm.id}>{farm.name}</div>)}
+    </>
+  );
+};
+
+export default Cart;
